6c97943abced6567c078284429cfd3ab67c1d932
icutter <damiano.tagliaferri@proton.me>
2025-10-17 13:00:20 +0200

    Add L02/E02

--- s342378_1/L02/E02/main.c
+++ s342378_1/L02/E02/main.c
@@ -1,18 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void malloc2dP(int ***mat)
+int **malloc2dR(FILE *fp, int nr, int nc)
 {
+    int **m = (int **)malloc(nr * sizeof(int *));
+    if (m == NULL)
+        exit(1);
+
+    for (int i = 0; i < nr; i++)
+    {
+        m[i] = (int *)malloc(nc * sizeof(int));
+        if (m[i] == NULL)
+            exit(1);
+
+        for (int j = 0; j < nc; j++)
+        {
+            fscanf(fp, "%d", &m[i][j]);
+        }
+    }
+
+    return m;
 }
 
-int **malloc2dR()
+void print_matrix(int **m, int nr, int nc)
 {
+    for (int i = 0; i < nr; i++)
+    {
+        for (int j = 0; j < nc; j++)
+        {
+            printf("%d\t", m[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-void separa(int **mat, int nr, int nc)
+void separa(int **mat, int nr, int nc, int **w, int *wc, int **b, int *bc)
 {
+    int wi = 0, bi = 0;
+
+    int size = nr * nc;
+    *bc = size / 2;
+    *wc = *bc + (size % 2);
+
+    *w = (int *)malloc(*wc * sizeof(int));
+    *b = (int *)malloc(*bc * sizeof(int));
+
+    if (*w == NULL || *b == NULL)
+    {
+        printf("Allocation failed.");
+        exit(1);
+    }
+
+    for (int i = 0; i < nr; i++)
+    {
+        for (int j = 0; j < nc; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                (*w)[wi] = mat[i][j];
+                wi++;
+            }
+            else
+            {
+                (*b)[bi] = mat[i][j];
+                bi++;
+            }
+        }
+    }
 }
 
-int main(int argc, int *argv[])
+void print_array(int *arr, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[])
 {
     FILE *fp = fopen("mat.txt", "r");
     if (fp == NULL)
@@ -23,7 +89,28 @@ int main(int argc, int *argv[])
 
     int nr, nc;
     fscanf(fp, "%d %d", &nr, &nc);
-    printf("Matrix size: %dx%d\n", nr, nc);
+    printf("Matrix size: %dx%d\n\n", nr, nc);
 
-    // int **mat = malloc2dR();
+    printf("Data in file:\n");
+    int **mat = malloc2dR(fp, nr, nc);
+
+    print_matrix(mat, nr, nc);
+
+    int *white_arr, white_len, *black_arr, black_len;
+    separa(mat, nr, nc, &white_arr, &white_len, &black_arr, &black_len);
+
+    // Deallocate matrix
+    for (int i = 0; i < nr; i++)
+    {
+        free(mat[i]);
+    }
+    free(mat);
+
+    printf("White len: %d | ", white_len);
+    print_array(white_arr, white_len);
+    free(white_arr);
+
+    printf("Black len: %d | ", black_len);
+    print_array(black_arr, black_len);
+    free(black_arr);
 }
