33607b6f004e500e6cb630bea7fa4a315650dbfc
icutter <damiano.tagliaferri@proton.me>
2025-10-29 01:03:24 +0100

    Update L03/E02

--- s342378_1/L03/E02/main.c
+++ s342378_1/L03/E02/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
+
+#define CODE_STR_LEN 6
+#define DATE_STR_LEN 11
 
 typedef struct
 {
@@ -9,7 +13,7 @@ typedef struct
 
 typedef struct
 {
-    char codice[51];
+    char codice[CODE_STR_LEN];
     char nome[51];
     char cognome[51];
     date_t data_di_nascita;
@@ -18,11 +22,32 @@ typedef struct
     int cap;
 } item_t;
 
-typedef struct
+typedef struct node
 {
     item_t value;
-    node_t *next;
+    struct node *next;
 } node_t;
+
+int parse_code(char *dest, char *str)
+{
+    // AXXXX
+    // 01234
+
+    if (strlen(str) != 5)
+        return 0;
+
+    if (str[0] != 'A')
+        return 0;
+
+    for (int i = 1; i <= 4; i++)
+    {
+        if (!isdigit(str[i]))
+            return 0;
+    }
+
+    strcpy(dest, str);
+    return 1;
+}
 
 int parse_date(date_t *date, char *str)
 {
@@ -68,54 +93,98 @@ void clear_list(node_t **head)
     }
 }
 
-int create_node(node_t **node, char *date)
+void print_item(item_t item)
 {
-    *node = malloc(sizeof(node_t));
-
-    char *codeStr;
-    char *dateStr;
-
-    // sscanf(date, "%5s %s %s %10s %s %s %d",
-
-    // );
+    perror("Function is not implemented.");
+    exit(1);
 }
 
-int insert_sorted(node_t **head, item_t value)
+void print_list(node_t *head)
 {
-    // node_t *new_node = malloc(sizeof(node_t));
-
-    // if (*head == NULL)
-    // {
-    //     new_node->value = new_item;
-    // }
+    node_t *current = head;
+    while (current)
+    {
+        print_item(current->value);
+        current = current->next;
+    }
 }
 
-int new_list_from_file(node_t **head, char *path)
+int parse_item(item_t **item, char *str)
+{
+    if (*item != NULL)
+    {
+        free(*item);
+    }
+    *item = malloc(sizeof(item_t));
+    if (*item == NULL)
+    {
+        perror("Memory allocation failure.");
+        return 0;
+    }
+
+    char codice_str[CODE_STR_LEN];
+    char data_str[DATE_STR_LEN];
+
+    int res = sscanf(str, "%5s %s %s %10s %s %s %d",
+                     codice_str,
+                     (*item)->nome,
+                     (*item)->cognome,
+                     data_str,
+                     (*item)->via,
+                     (*item)->citta,
+                     &(*item)->cap);
+
+    if (res != 7)
+    {
+        return 0;
+    }
+
+    if (!parse_code((*item)->codice, codice_str))
+    {
+        return 0;
+    }
+
+    if (!parse_date(&(*item)->data_di_nascita, data_str))
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+int add_to_sorted_list(node_t **head, item_t item)
+{
+    node_t *new_node = malloc(sizeof(node_t));
+    new_node->value = item;
+
+    // Check first item
+
+    node_t *current = *head;
+    while (comp_date(current->value.data_di_nascita, new_node->value.data_di_nascita))
+    {
+        current = current->next;
+    }
+
+    // Insert node
+}
+
+int add_from_file(node_t **head, char *path)
 {
     FILE *fp = fopen(path, "r");
     if (fp == NULL)
     {
         printf("Could not open file.");
-        return 1;
+        return 0;
     }
 
-    int scanned = 0;
-
-    while (1)
+    char line[256];
+    while (fgets(line, sizeof(line), fp))
     {
         item_t *new_item = malloc(sizeof(item_t));
-        if (new_item == NULL)
+        if (parse_item(&new_item, line))
         {
+            // add_to_sorted_list();
         }
-
-        scanned = fscanf(fp, "%5s %s %s %10s %s %s %d",
-                         new_item->codice,
-                         new_item->nome,
-                         new_item->cognome,
-                         new_item->data_di_nascita,
-                         new_item->via,
-                         new_item->citta,
-                         &new_item->cap);
     }
 }
 
@@ -124,7 +193,7 @@ int main(int argc, char *argv[])
     node_t *head = NULL;
     char path[] = "anag1.txt";
 
-    new_list_from_file(&head, path);
+    add_from_file(&head, path);
 
     return 0;
 }
