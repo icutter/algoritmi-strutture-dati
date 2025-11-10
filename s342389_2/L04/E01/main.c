055bfb9c2651602e88150d9866c410cbdf3ddc06
icutter <damiano.tagliaferri@proton.me>
2025-11-11 00:37:07 +0100

    Add L04/E01

--- s342389_2/L04/E01/main.c
+++ s342389_2/L04/E01/main.c
@@ -6,26 +6,17 @@ typedef struct
     int u, v;
 } edge_t;
 
-int powerset(int pos, int *val, int *sol, int n, int cnt)
+void print_data(edge_t *arr, int N, int E)
 {
-    int j;
-    if (pos >= n)
+    printf("\nN: %d\tE: %d\n\n", N, E);
+
+    for (int i = 0; i < E; i++)
     {
-        printf("{ \t");
-        for (j = 0; j < n; j++)
-            if (sol[j] != 0)
-                printf("%d \t", val[j]);
-        printf("} \n");
-        return cnt + 1;
+        printf("%d %d\n", arr[i].u, arr[i].v);
     }
-    sol[pos] = 0;
-    cnt = powerset(pos + 1, val, sol, n, cnt);
-    sol[pos] = 1;
-    cnt = powerset(pos + 1, val, sol, n, cnt);
-    return cnt;
 }
 
-void load_file(edge_t *arr, int *N, int *E)
+int load_file(edge_t **arr, int *N, int *E)
 {
     FILE *fp = fopen("grafo.txt", "r");
     if (fp == NULL)
@@ -36,14 +27,14 @@ void load_file(edge_t *arr, int *N, int *E)
 
     fscanf(fp, "%d %d", N, E);
 
-    arr = malloc(*N * sizeof(edge_t));
+    *arr = malloc(*E * sizeof(edge_t));
 
     edge_t new_edge;
-    for (int i = 0; i < *N; i++)
+    for (int i = 0; i < *E; i++)
     {
         if (fscanf(fp, "%d %d", &new_edge.u, &new_edge.v) == 2)
         {
-            arr[i] = new_edge;
+            (*arr)[i] = new_edge;
         }
         else
         {
@@ -55,13 +46,60 @@ void load_file(edge_t *arr, int *N, int *E)
     return 1;
 }
 
+int contains(int *arr, int n, int val)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == val)
+            return 1;
+    }
+    return 0;
+}
+
+int is_vertex_cover(edge_t *arr, int E, int *cover, int C)
+{
+    for (int i = 0; i < E; i++)
+    {
+        if (!contains(cover, C, arr[i].u) && !contains(cover, C, arr[i].v))
+            return 0;
+    }
+    return 1;
+}
+
+void print_vertex_covers_rec(edge_t *edges, int N, int E, int idx, int *cover, int count)
+{
+    if (idx == N)
+    {
+        // abbiamo considerato tutti i vertici, controlliamo se è un vertex cover
+        if (is_vertex_cover(edges, E, cover, count))
+        {
+            printf("{ ");
+            for (int i = 0; i < count; i++)
+                printf("%d ", cover[i]);
+            printf("}\n");
+        }
+        return;
+    }
+
+    // 1) includo idx nel cover
+    cover[count] = idx;
+    print_vertex_covers_rec(edges, N, E, idx + 1, cover, count + 1);
+
+    // 2) non includo idx nel cover
+    print_vertex_covers_rec(edges, N, E, idx + 1, cover, count);
+}
+
 int main(int argc, char *argv[])
 {
-    edge_t *arr;
+    edge_t *edges;
     int N; // vertex count
     int E; // edge count
 
-    load_file(arr, &N, &E);
+    load_file(&edges, &N, &E);
+    // print_data(edges, E, N);
+
+    int *cover = malloc(N * sizeof(int));
+    print_vertex_covers_rec(edges, N, E, 0, cover, 0);
 
     return 0;
 }
