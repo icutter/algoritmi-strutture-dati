f49d4d228648aabeeb2c76f181b441f35f3c2b9e
icutter <damiano.tagliaferri@proton.me>
2025-11-30 23:15:16 +0100

    Update L05/E02

--- s342389_2/L05/E02/main.c
+++ s342389_2/L05/E02/main.c
@@ -71,26 +71,73 @@ void read_board_start(char *path, cell_t ***BOARD, int *R, int *C)
     fclose(fp);
 }
 
-int get_score(cell_t **BOARD, int R, int C)
+// axis = 0 -> orizzontale (col1/val1)
+// axis = 1 -> verticale   (col2/val2)
+static inline char get_color(tile_t t, int rot, int axis)
 {
-    int total_score = 0;
+    if (!rot)
+        return axis == 0 ? t.col1 : t.col2;
+    else
+        return axis == 0 ? t.col2 : t.col1;
+}
 
+static inline int get_value(tile_t t, int rot, int axis)
+{
+    if (!rot)
+        return axis == 0 ? t.val1 : t.val2;
+    else
+        return axis == 0 ? t.val2 : t.val1;
+}
+
+int score_line(cell_t **BOARD, int R, int C, tile_t *TILES,
+               int index, int axis, int is_row)
+{
+    int len = is_row ? C : R;
+
+    // primo elemento della linea
+    cell_t first = is_row ? BOARD[index][0] : BOARD[0][index];
+    tile_t tf = TILES[first.tile];
+
+    // 0 = orizzontale per righe, 1 = verticale per colonne
+    char base_color = get_color(tf, first.rot, axis);
+    int sum = get_value(tf, first.rot, axis);
+
+    for (int k = 1; k < len; k++)
+    {
+        cell_t c = is_row ? BOARD[index][k] : BOARD[k][index];
+        tile_t t = TILES[c.tile];
+
+        char col = get_color(t, c.rot, axis);
+        int val = get_value(t, c.rot, axis);
+
+        if (col != base_color)
+            return 0; // linea non valida
+
+        sum += val;
+    }
+
+    return sum;
+}
+
+int get_score(cell_t **BOARD, int R, int C, tile_t *TILES)
+{
+    int total = 0;
+
+    // RIGHE -> axis = 0 (orizzontale)
     for (int i = 0; i < R; i++)
-    {
-        cell_t current = BOARD[i][0];
-        // char color = current.color1;
+        total += score_line(BOARD, R, C, TILES, i, 0, 1);
 
-        for (int j = 1; j < C; j++)
-        {
-        }
-    }
-
+    // COLONNE -> axis = 1 (verticale)
     for (int j = 0; j < C; j++)
-    {
-        for (int i = 0; i < R; i++)
-        {
-        }
-    }
+        total += score_line(BOARD, R, C, TILES, j, 1, 0);
+
+    return total;
+}
+
+void find_best_score_rec(cell_t **BOARD, int R, int C, tile_t *TILES)
+{
+    // TODO: Implement recursive function
+    // get_score(BOARD, R, C, TILES);
 }
 
 int main(int argc, char **argv)
@@ -103,6 +150,8 @@ int main(int argc, char **argv)
 
     read_available_tiles("tiles.txt", &TILES, &T);
     read_board_start("board.txt", &BOARD, &R, &C);
+
+    find_best_score_rec(BOARD, R, C, TILES);
 
     return 0;
 }
